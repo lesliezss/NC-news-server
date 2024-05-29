506a49ac20cc6cde917a11ab9d3b94c03e5608f8
Lesliezss <you@example.com>
2024-05-29 09:44:41 +0100

    added get api endpoint

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -1,4 +1,5 @@
 const { selectTopics } = require("../models/app.models");
+const endpointsData = require("../endpoints.json");
 
 exports.getTopics = (req, res, next) => {
   return selectTopics()
@@ -9,4 +10,8 @@ exports.getTopics = (req, res, next) => {
       console.error("Error caught in getTopics:", err);
       next(err);
     });
+};
+
+exports.getEndpointsData = (req, res, next) => {
+  return res.status(200).send({ endpointsData });
 };
