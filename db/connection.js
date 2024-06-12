ea4c077c6f9df80c0c546791522e0a1eb2611e03
Lesliezss <you@example.com>
2024-06-12 17:03:11 +0100

    make changes for hosting1

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
+
+
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
@@ -8,7 +10,6 @@ require('dotenv').config({
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
-
 
 const config = {};
 
