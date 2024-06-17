8df98616e7d9bac5da3cf57506092074092510f7
Lesliezss <you@example.com>
2024-06-17 13:18:13 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-const { 
-    getTopics, 
-    getEndpointsData,
-    getArticleById, 
-    getArticles, 
-    getCommentsByArticleId,
-    addComment,
-    updateArticleById,
-    deleteCommentById
+const {
+  getTopics,
+  getEndpointsData,
+  getArticleById,
+  getArticles,
+  getCommentsByArticleId,
+  addComment,
+  updateArticleById,
+  deleteCommentById,
 } = require("./controllers/app.controller");
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -17,32 +20,32 @@ app.get("/api/topics", getTopics);
 
 app.get("/api", getEndpointsData);
 
-app.get("/api/articles/:article_id",getArticleById)
+app.get("/api/articles/:article_id", getArticleById);
 
-app.get("/api/articles", getArticles)
+app.get("/api/articles", getArticles);
 
-app.get("/api/articles/:article_id/comments",getCommentsByArticleId)
+app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 
-app.post("/api/articles/:article_id/comments", addComment)
+app.post("/api/articles/:article_id/comments", addComment);
 
-app.patch("/api/articles/:article_id",updateArticleById)
+app.patch("/api/articles/:article_id", updateArticleById);
 
-app.delete("/api/comments/:comment_id", deleteCommentById)
+app.delete("/api/comments/:comment_id", deleteCommentById);
 
-app.use((err, req, res, next)=>{
-    if(err.status){
-        res.status(err.status).send({msg: err.msg})
-    } else next(err)
-})
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+});
 
-app.use((err, req, res, next)=>{
-    if(err.code === '22P02'){
-        res.status(400).send({msg:'Invalid Input'})
-    }else next(err)
-})
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Invalid Input" });
+  } else next(err);
+});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ msg: "Internal Server Error" });
 });
 
-module.exports =  app;
+module.exports = app;
