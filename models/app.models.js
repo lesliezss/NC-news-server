c2110fd76dfa7053721dcc24bec9b124e91da7e0
Lesliezss <you@example.com>
2024-06-20 16:00:34 +0100

    added oeder by time when adding comment

--- models/app.models.js
+++ models/app.models.js
@@ -80,59 +80,73 @@ function addCommentModel(username, body, article_id) {
           msg: `Username not found`,
         });
       }
-      return db
-        .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
-      })
 
-      .then((result) => {
-        if (!result.rows.length) {
-          return Promise.reject({
-            status: 404,
-            msg: `No article found for article id: ${article_id}`,
-          });
-        }
-        return db.query(
-          `
+      return db.query(`SELECT * FROM articles WHERE article_id = $1`, [
+        article_id,
+      ]);
+    })
+
+    .then((result) => {
+      if (!result.rows.length) {
+        return Promise.reject({
+          status: 404,
+          msg: `No article found for article id: ${article_id}`,
+        });
+      }
+
+      return db.query(
+        `
           INSERT INTO comments (body, author, article_id, votes, created_at)
-          VALUES ($1, $2, $3, $4, Now()) RETURNING *`,
-          [body, username, article_id, 0]
-        );
-      })
-
+          VALUES ($1, $2, $3, $4, Now()) RETURNING * `,
+        [body, username, article_id, 0]
+      );
+    })
+    .then(() => {
+      return db.query(
+        `
+          SELECT * FROM comments
+          WHERE article_id = $1
+          ORDER BY created_at DESC
+        `,
+        [article_id]
+      );
+    })
     .then((result) => {
       return result.rows[0];
     });
 }
 
-function updateArticleByIdModel (article_id, inc_votes){
-  return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
-  .then((result)=>{
-    if(!result.rows.length){
-      return Promise.reject({
-        status: 404,
-        msg: `No article found for article id: ${article_id}`,
-      })
-    }
-    return db.query(`UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *`,
-    [inc_votes, article_id])
-  }
-)
-.then((result)=>{
-  return (result.rows[0])
-})
+function updateArticleByIdModel(article_id, inc_votes) {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then((result) => {
+      if (!result.rows.length) {
+        return Promise.reject({
+          status: 404,
+          msg: `No article found for article id: ${article_id}`,
+        });
+      }
+      return db.query(
+        `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *`,
+        [inc_votes, article_id]
+      );
+    })
+    .then((result) => {
+      return result.rows[0];
+    });
 }
 
-
-function deleteCommentByIdModel (comment_id){
-return db.query(`DELETE FROM comments WHERE comment_id = $1`, [comment_id])
-.then((result)=>{
-  if(result.rowCount===0){
-    return Promise.reject({
-      status: 404,
-      msg: `No comment found on comment id: ${comment_id}`,
-    })
-  }
-})
+function deleteCommentByIdModel(comment_id) {
+  return db
+    .query(`DELETE FROM comments WHERE comment_id = $1`, [comment_id])
+    .then((result) => {
+      if (result.rowCount === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: `No comment found on comment id: ${comment_id}`,
+        });
+      }
+    });
 }
 
 module.exports = {
@@ -142,5 +156,5 @@ module.exports = {
   selectCommentsByArticleId,
   addCommentModel,
   updateArticleByIdModel,
-  deleteCommentByIdModel
+  deleteCommentByIdModel,
 };
